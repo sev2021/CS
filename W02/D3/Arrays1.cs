0c9948897688913464b741dd820c55e70204fc06
Sev <90162122+sev2021@users.noreply.github.com>
2022-09-28 11:20:09 +0100

    final

--- W02/D3/Arrays1.cs
+++ W02/D3/Arrays1.cs
@@ -12,17 +12,18 @@ namespace Array1
             //declare arrays
 
             int[] numbers1 = { 1, 2, 3, 4, 5, 6 };
-            int[] numbers2 = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] numbers2 = new int[] { 21,52, 63, 884, 55, 26 };
             int[] numbers3 = new int[5];
 
             string [] numbers4;
             numbers4 = new string[5];
             numbers4[4] = "abc";
 
-            Console.WriteLine(numbers4[2+2].Length + "\n");
+            for (int i = 0; i < numbers2.Length; i++)
+                Console.WriteLine($"Element {i + 1} = {numbers2[i]}");
 
-            foreach (int i in numbers1) Console.WriteLine(i);
-            foreach (string i in numbers4) Console.WriteLine(i);
+            Console.WriteLine(numbers2.Sum());
         }
     }
 }
+
