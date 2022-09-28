38f99145611d9aeeba52d5b2e917f4bd565c11e2
Sev <90162122+sev2021@users.noreply.github.com>
2022-09-28 11:57:49 +0100

    Array of strings

---
+++ W02/D3/StringArrays.cs
@@ -0,0 +1,21 @@
+// 28/09/2022
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace StringArrays1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words1 = { "Aaaaa", "Bbbbb", "Cccc" };
+            string[] words2 = { "", "" };
+
+        }
+    }
+}
